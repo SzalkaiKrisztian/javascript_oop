9cc64669f7c1e9c08bb1e54995d66a906485604b
Szalkai Krisztian Peter <szalkai.krisztian@BOLYAI.LOCAL>
2026-02-06 13:10:03 +0100

    ora vege

---
+++ col_row/manager.js
@@ -0,0 +1,33 @@
+/**
+ * @import {FormFieldType,HeaderArrayType,ColspanType,RowspanType} from './functions.js' 
+ *
+ * @callback addCallback
+ * @param {ColspanType | RowspanType} 
+ * @returns {void}
+ * */
+class Manager{
+    /**@type {ColspanType[] | RowspanType[]} */
+    #dataArry;
+    /**@type {addCallback} */
+    #addCallback;
+    /**@param {addCallback} value */
+    set addCallback(value){
+        this.#addCallback=value
+    }
+    constructor(){
+        this.#dataArry=[]
+    }
+    /**
+     * 
+     * @param {ColspanType | RowspanType} colRowType 
+     * @returns {void}
+     */
+    addElement(colRowType){
+        this.#dataArry.push(colRowType)
+        if(this.#addCallback){this.#addCallback(colRowType)}
+    }
+}
+
+
+
+export {Manager}
