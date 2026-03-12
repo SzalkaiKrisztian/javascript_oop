63ed35ff7e08a8de189dc60313f4371b12bb06f0
krisztian <szalkai.krisztian_2022c@bolyai-szakkozep.hu>
2026-03-12 20:32:54 +0100

    tul sok uj minden, egyszer hianyoztam aztan bumm leves az egesz

---
+++ AuthorManager/formview.js
@@ -0,0 +1,10 @@
+import { ViewElement } from "./viewelement.js";
+
+class FormView extends ViewElement{
+    constructor(id){
+        super(id);
+        this.div.innerText= "Form"
+    }
+}
+
+export {FormView}
